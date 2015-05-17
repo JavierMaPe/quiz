486d3889683a17d52e6c01552a17c451417b2bcd
JavierMaPe <raivak_one@hotmail.com>
2015-05-17 17:39:43 +0200

    crea preguntas

--- routes/index.js
+++ routes/index.js
@@ -11,9 +11,15 @@ router.get('/', function(req, res) {
 //router.get('/quizes/question', quizController.question);
 //router.get('/quizes/answer', quizController.answer);
 
+//Autoload de comandos con :quizId
+router.param('quizId', quizController.load); //autoload :quizId
+
+//definición de rutas
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new', quizController.new);
+router.post('/quizes/create', quizController.create);
 
 router.get('/author', function(req, res) {
    res.render('author', {title: 'creditos' });
